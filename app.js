1aca677d25cca92d068030e0186f5f29e4ca703d
André Lademann <andre@programmerq.eu>
2014-09-10 19:44:26 +0200

    Intial commit with yo express

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+  config = require('./config/config'),
+  fs = require('fs'),
+  mongoose = require('mongoose');
+
+mongoose.connect(config.db);
+var db = mongoose.connection;
+db.on('error', function () {
+  throw new Error('unable to connect to database at ' + config.db);
+});
+
+var models = glob.sync(config.root + '/app/models/*.js');
+models.forEach(function (model) {
+  require(model);
+});
+var app = express();
+
+require('./config/express')(app, config);
+
+app.listen(config.port);
+
