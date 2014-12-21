f301ff81949c324dd39c2908092f253d6662a414
André Lademann <vergissberlin@googlemail.com>
2014-12-22 00:11:56 +0100

    Add deploy pull script

---
+++ lib/pull.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Pizza
+#
+# LICENSE:    MIT
+#
+# @project    foosball
+# @package    ChromeExtension
+# @author     André Lademann <vergissberlin@googlemail.com>
+# @copyright  Copyright (c) 2014 andrelademann.de (http://www.andrelademann.de)
+# @license    http://andrelademann.de/license
+# @since      2014-12-18 - 11:53:21 PM
+
+
+# 1. Update repository
+cd ~/foosball &&\
+git checkout develop &&\
+git pull &&\
+
+
+# 2. Create backup
+cat ../node-red/flows_foosball.json > ../node-red/flows_backup.json &&\
+
+# 3. Copy flow file and change user
+cat server/home/pi/node-red/flows_foosball.json > ../node-red/flows_foosball.json &&\
+chown pi ../node-red/flows_foosball.json &&\
+
+# 4. Push and log
+git push >> ~/deploy.log
