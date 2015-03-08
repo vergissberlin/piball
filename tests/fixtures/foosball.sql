4728eb3e97708c30e64130684e07c9281eb09d14
André Lademann <vergissberlin@googlemail.com>
2015-03-08 17:44:08 +0100

    Use file fixtures

--- tests/fixtures/foosball.sql
+++ tests/fixtures/foosball.sql
@@ -1,8 +1,8 @@
 /* Create table */
-CREATE TABLE foosball.score (
+CREATE TABLE IF NOT EXISTS foosball.score (
 	id					INTEGER(10) UNSIGNED AUTO_INCREMENT,
 	type				VARCHAR(24),
 	updated_at	TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	created_at	DATETIME DEFAULT NULL,
 	PRIMARY KEY (id)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
