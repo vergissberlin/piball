054dfa88fbdec73b5f286f8ed24408b7f27169fb
André Lademann <vergissberlin@googlemail.com>
2015-03-08 12:14:08 +0100

    Test post install hook

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,5 +2,7 @@ var sys = require('sys');
 var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 console.info('✪ Install OS dependencies ✪');
+/*
 exec("sudo aptitude update", puts);
 exec("sudo aptitude install mc git mysql-server", puts);
+*/
