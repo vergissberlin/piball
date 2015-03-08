0e38d78dd229b908a26a31e17dd5ed367232dbf6
André Lademann <vergissberlin@googlemail.com>
2015-03-08 13:02:27 +0100

    Uninstall works fine. Let's try the others.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,8 +1,4 @@
 var sys = require('sys');
 var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { sys.puts(stdout) }
-console.info('✪ Install OS dependencies ✪');
-/*
-exec("sudo aptitude update", puts);
-exec("sudo aptitude install mc git mysql-server", puts);
-*/
+console.info('✪ OS dependencies installed ✪');
