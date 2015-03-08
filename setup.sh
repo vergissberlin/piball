a95bdfece8a052a742178955463f62b6ae0bf684
André Lademann <vergissberlin@googlemail.com>
2015-03-08 17:28:48 +0100

    Create fixtures for travis ci

--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get upgrade
-apt-get install mc git gpio
---
+++ tests/fixtures/foosball.sql
@@ -0,0 +1,8 @@
+/* Create table */
+CREATE TABLE foosball.score (
+	id					INTEGER(10) UNSIGNED AUTO_INCREMENT,
+	type				VARCHAR(24),
+	updated_at	TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	created_at	DATETIME DEFAULT NULL,
+	PRIMARY KEY (id)
+);
