15de927de24db63b41e88b4ce6c57764e43f8929
André Lademann <vergissberlin@googlemail.com>
2015-03-08 16:15:55 +0100

    Remove install scripts

--- scripts/postinstall.js
+++
@@ -1,4 +0,0 @@
-var sys = require('sys');
-var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { sys.puts(stdout) }
-console.info('✪ OS dependencies installed ✪');
--- scripts/uninstall.js
+++
@@ -1,5 +0,0 @@
-var sys = require('sys');
-var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { sys.puts(stdout) }
-console.info('✪ Uninstall OS dependencies ✪');
-exec("ls -la", puts);
