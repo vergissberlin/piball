43635a26e10aefd1a40abdef5d04073a816b2876
André Lademann <vergissberlin@googlemail.com>
2015-03-08 12:01:50 +0100

    Use sudo for install script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,5 +2,5 @@ var sys = require('sys');
 var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 console.info('✪ Install OS dependencies ✪');
-exec("aptitude update", puts);
-exec("aptitude install mc git mysql-server", puts);
+exec("sudo aptitude update", puts);
+exec("sudo aptitude install mc git mysql-server", puts);
