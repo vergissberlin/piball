6d27381af5623ced1bf166ff3108a29f33aee0ee
André Lademann <vergissberlin@googlemail.com>
2015-04-24 14:24:37 +0200

    Add release script to publish flow to npmjs.org

---
+++ scripts/release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Foosball
+#
+# LICENSE:    MIT
+#
+# @project    foosball
+# @package    deployment
+# @author     André Lademann <vergissberlin@googlemail.com>
+# @copyright  Copyright (c) 2014 andrelademann.de (http://www.andrelademann.de)
+# @license    http://andrelademann.de/license
+# @since      2015-04-23 - 13:53:21 PM
+
+
+# 1. Deploy current state
+cd ~/foosball &&\
+sh scripts/deploy.sh
+
+git checkout master &&\
+git pull &&\
+
+# 2. Publish to npmjs.org
+npm version patch -m "Upgrade to %s because the node-red flow was updated"
+npm publish ./
+
+exit 0;
